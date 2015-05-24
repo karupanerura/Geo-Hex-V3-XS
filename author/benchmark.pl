ca78285ae61a547413bf2506320ad8a207d13dfd
karupanerura <karupa@cpan.org>
2015-05-24 10:16:53 +0900

    Updated benchmark.

--- author/benchmark.pl
+++ author/benchmark.pl
@@ -25,6 +25,7 @@ my @geohexes = qw/
     XM48827674605123
     XM488276746051234
 /;
+my (@latlngs) = map { [Geo::Hex::V3::XS::decode_geohex($_)] } @geohexes;
 
 for my $geohex (@geohexes) {
     my $zone_pp = Geo::Hex::V3::geohex2zone($geohex);
@@ -40,7 +41,9 @@ for my $geohex (@geohexes) {
     };
 }
 
-cmpthese timethese 10000 => {
+print '-' x 60, "\n";
+print 'Creates zone object from geohex:', "\n";
+cmpthese timethese 20000 => {
     pp => sub {
         Geo::Hex::V3::geohex2zone($_) for @geohexes;
     },
@@ -48,6 +51,43 @@ cmpthese timethese 10000 => {
         Geo::Hex::V3::XS->new(code => $_) for @geohexes;
     },
 };
+print '-' x 60, "\n";
+
+print '-' x 60, "\n";
+print 'Creates zone object from latlng:', "\n";
+cmpthese timethese 20000 => {
+    pp => sub {
+        Geo::Hex::V3::latlng2zone(@$_) for @latlngs;
+    },
+    xs => sub {
+        Geo::Hex::V3::XS->new(lat => $_->[0], lng => $_->[1], level => $_->[2]) for @latlngs;
+    },
+};
+print '-' x 60, "\n";
+
+print '-' x 60, "\n";
+print 'Converts latlng to geohex:', "\n";
+cmpthese timethese 20000 => {
+    pp => sub {
+        Geo::Hex::V3::latlng2geohex(@$_) for @latlngs;
+    },
+    xs => sub {
+        Geo::Hex::V3::XS::encode_geohex(@$_) for @latlngs;
+    },
+};
+print '-' x 60, "\n";
+
+print '-' x 60, "\n";
+print 'Converts geohex to latlng:', "\n";
+cmpthese timethese 20000 => {
+    pp => sub {
+        Geo::Hex::V3::geohex2latlng($_) for @geohexes;
+    },
+    xs => sub {
+        Geo::Hex::V3::XS::decode_geohex($_) for @geohexes;
+    },
+};
+print '-' x 60, "\n";
 
 __END__
 1..16
@@ -211,9 +251,39 @@ ok 15 - XM48827674605123
     ok 7 - size
     1..7
 ok 16 - XM488276746051234
-Benchmark: timing 10000 iterations of pp, xs...
-        pp: 10 wallclock secs (10.18 usr +  0.04 sys = 10.22 CPU) @ 978.47/s (n=10000)
-        xs:  1 wallclock secs ( 0.78 usr +  0.01 sys =  0.79 CPU) @ 12658.23/s (n=10000)
+------------------------------------------------------------
+Creates zone object from geohex:
+Benchmark: timing 20000 iterations of pp, xs...
+        pp: 14 wallclock secs (13.77 usr +  0.14 sys = 13.91 CPU) @ 1437.81/s (n=20000)
+        xs:  1 wallclock secs ( 1.21 usr +  0.01 sys =  1.22 CPU) @ 16393.44/s (n=20000)
       Rate    pp    xs
-pp   978/s    --  -92%
-xs 12658/s 1194%    --
+pp  1438/s    --  -91%
+xs 16393/s 1040%    --
+------------------------------------------------------------
+------------------------------------------------------------
+Creates zone object from latlng:
+Benchmark: timing 20000 iterations of pp, xs...
+        pp: 15 wallclock secs (14.43 usr +  0.15 sys = 14.58 CPU) @ 1371.74/s (n=20000)
+        xs:  1 wallclock secs ( 1.26 usr +  0.01 sys =  1.27 CPU) @ 15748.03/s (n=20000)
+      Rate    pp    xs
+pp  1372/s    --  -91%
+xs 15748/s 1048%    --
+------------------------------------------------------------
+------------------------------------------------------------
+Converts latlng to geohex:
+Benchmark: timing 20000 iterations of pp, xs...
+        pp: 15 wallclock secs (14.34 usr +  0.15 sys = 14.49 CPU) @ 1380.26/s (n=20000)
+        xs:  1 wallclock secs ( 0.92 usr +  0.01 sys =  0.93 CPU) @ 21505.38/s (n=20000)
+      Rate    pp    xs
+pp  1380/s    --  -94%
+xs 21505/s 1458%    --
+------------------------------------------------------------
+------------------------------------------------------------
+Converts geohex to latlng:
+Benchmark: timing 20000 iterations of pp, xs...
+        pp: 15 wallclock secs (14.24 usr +  0.15 sys = 14.39 CPU) @ 1389.85/s (n=20000)
+        xs:  1 wallclock secs ( 1.04 usr +  0.01 sys =  1.05 CPU) @ 19047.62/s (n=20000)
+      Rate    pp    xs
+pp  1390/s    --  -93%
+xs 19048/s 1270%    --
+------------------------------------------------------------
