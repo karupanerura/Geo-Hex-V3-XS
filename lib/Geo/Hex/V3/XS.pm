809fb2423541ce532a8227038cf2ac3bd88b5219
karupanerura <karupa@cpan.org>
2015-06-06 10:08:59 +0900

    Checking in changes prior to tagging of version 0.06.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index bf9f36e..c6ca7ce 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Geo-Hex-V3-XS
    
     {{$NEXT}}
    
    +0.06 2015-06-06T01:08:43Z
    +
    +   - Fixed #1 test expected value for `-Duselongdouble` perl.
    +
     0.05 2015-06-03T03:43:00Z
    
        - Fixed dependency.

--- lib/Geo/Hex/V3/XS.pm
+++ lib/Geo/Hex/V3/XS.pm
@@ -3,7 +3,7 @@ use 5.008001;
 use strict;
 use warnings;
 
-our $VERSION = "0.05";
+our $VERSION = "0.06";
 use Exporter 5.57 qw/import/;
 our @EXPORT_OK = qw/encode_geohex decode_geohex geohex_hexsize/;
 
