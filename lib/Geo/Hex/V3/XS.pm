6d82cd6e6a9997d9f8c462b29f02c84403d22ca1
karupanerura <karupa@cpan.org>
2015-08-15 18:27:44 +0900

    Checking in changes prior to tagging of version 0.08.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 8ab0305..275d9b5 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Geo-Hex-V3-XS
    
     {{$NEXT}}
    
    +0.08 2015-08-15T09:25:49Z
    +
    +   - Upgrade c-geohex3 to 1.40. (improvement of accuracy)
    +
     0.07 2015-06-22T23:45:38Z
    
        - Fixed compile option.

--- lib/Geo/Hex/V3/XS.pm
+++ lib/Geo/Hex/V3/XS.pm
@@ -3,7 +3,7 @@ use 5.008001;
 use strict;
 use warnings;
 
-our $VERSION = "0.07";
+our $VERSION = "0.08";
 use Exporter 5.57 qw/import/;
 our @EXPORT_OK = qw/encode_geohex decode_geohex geohex_hexsize/;
 
